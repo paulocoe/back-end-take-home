b9b1cb0c9eeffd313312038cebe33f9ef6c20177
paulo filho <Paulo.Filho@infinite.software>
2019-09-09 14:46:40 +0100

    Creates Web API and tests

---
+++ TakeHome.Web.Api/Controllers/TakeHomeController.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TakeHome.Mediator.Requests;
+
+
+namespace TakeHome.Web.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/TakeHome")]
+    public class TakeHomeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TakeHomeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get(GetRequest request)
+        {
+            var response = await _mediator.Send(new GetShortestRouteRequest(request?.Origin, request?.Destination));
+
+            if (!string.IsNullOrEmpty(response.ErrorMessage))
+                return StatusCode(StatusCodes.Status500InternalServerError, response.ErrorMessage);
+
+            if (!response.IsValid)
+                return BadRequest(response.Content);
+
+            if (response.HasNoRoute)
+                return NotFound(response.Content);
+
+            return Ok(response.Content);
+        }
+    }
+}
---
+++ TakeHome.Web.Api/Startup.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TakeHome.Data;
+using TakeHome.Mediator.Handlers;
+using TakeHome.Services;
+
+namespace TakeHome
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ITakeHomeRepository, TakeHomeRepository>(x => new TakeHomeRepository(Configuration["ConnectionString"]));
+            services.AddScoped<ITakeHomeService, TakeHomeService>();            
+            services.AddMvc();
+            services.AddMediatR(typeof(GetShortestRouteHandler));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();            
+        }
+    }
+}
